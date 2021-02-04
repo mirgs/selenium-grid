8e7d778514164d08e00e0230f37331d815bd0470
Administrator <admin@example.com>
2021-02-04 21:24:53 +0100

    Archivos para test Junt

---
+++ hello-selenium-java/src/test/java/com/example/hello_selenium_java/ImdbTestRemoteOpera.java
@@ -0,0 +1,48 @@
+package com.example.hello_selenium_java;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class ImdbTestRemoteOpera {
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+    @BeforeEach
+    public void setUp() {
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+    }
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void imdb() throws MalformedURLException{
+
+        OperaOptions operaOptions = new OperaOptions();
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444"), operaOptions);
+        driver.get("https://www.imdb.com/");
+        driver.manage().window().setSize(new Dimension(907, 886));
+        driver.findElement(By.id("suggestion-search")).sendKeys("wandavision");
+        driver.findElement(By.id("suggestion-search")).sendKeys(Keys.ENTER);
+        WebElement we = new WebDriverWait(driver, 15)
+                .until(ExpectedConditions.elementToBeClickable(By.linkText("WandaVision")));
+        we.click();
+        //driver.findElement(By.linkText("WandaVision")).click();
+        we = new WebDriverWait(driver, 15)
+                .until(ExpectedConditions.elementToBeClickable(By.linkText("TRIVIA")));
+        we.click();
+    }
+}
